60a5e3931e50fd85db80d0bfea7dc33f0468bba0
Lazlo <glebite@gmail.com>
2019-07-02 21:55:29 -0400

    Fleshing out some more code tonight...

--- src/grid.py
+++ src/grid.py
@@ -15,6 +15,13 @@ class grid:
             self.rows = 10
             self.cols = 10
 
+    def createGrid(self):
+        if self.rows <= 0 or self.cols <= 0:
+            raise ValueError("rows or cols cannot be <= 0")
+        self.board = [[0 for x in range(self.cols)]
+                      for y in range(self.rows)]
+        return True
+            
 if __name__ == "__main__":
     print("Executing tests...")
     x = grid()
@@ -23,6 +30,14 @@ if __name__ == "__main__":
     y = grid((5,3))
     assert y.rows == 5
     assert y.cols == 3
+    assert y.createGrid() == True
+    z = grid((-1,5))
+    try:
+        assert z.createGrid() == True
+    except ValueError as e:
+        print("Exception raised as expected: {}".format(e))
+    
+    
     
 
             

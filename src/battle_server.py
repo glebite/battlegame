1f387edd660c11280eb9465f117ffb05b878cd6a
Lazlo <glebite@gmail.com>
2019-07-07 21:02:48 -0400

    started out basic server code and pep8'd it...

--- src/battle_server.py
+++ src/battle_server.py
@@ -1,6 +1,18 @@
 """
+battle_server.py
 """
 from flask import Flask
+from flask import jsonify
+
+APP = Flask(__name__)
+
+@APP.route('/status')
+def get_status():
+    """ get_status """
+    return jsonify(
+        system_status="ok",
+        game_status=""
+        )
 
 if __name__ == "__main__":
-    pass
+    APP.run(host='0.0.0.0', port=5150)

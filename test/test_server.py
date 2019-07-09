eaa3c39a89c03ae818af4fa755e18db47b782815
Lazlo <glebite@gmail.com>
2019-07-08 21:54:26 -0400

    as always, run things through pylint...

--- test/test_server.py
+++ test/test_server.py
@@ -1,9 +1,11 @@
+"""
+test_server.py
+"""
 import pytest
 import requests
 
 @pytest.mark.test_id(1)
 def test_status():
+    """ test_status """
     response = requests.get('http://127.0.0.1:5150/status')
-    assert response.status_code is 200
-    
-        
+    assert response.status_code == 200

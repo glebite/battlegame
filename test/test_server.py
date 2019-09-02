c48a3d9cf01430206ffefbc1d05198243c4b95f2
Lazlo <glebite@gmail.com>
2019-09-02 17:19:25 -0400

    tests!

--- test/test_server.py
+++ test/test_server.py
@@ -9,8 +9,9 @@ test_last
 """
 import pytest
 import requests
+import subprocess
 
-@pytest.mark.test_id(1)
+@pytest.mark.test_id(2)
 def test_status():
     """ test_status
     Confirms that the running server will return a 200 status_code
@@ -19,6 +20,13 @@ def test_status():
     response = requests.get('http://127.0.0.1:5150/status')
     assert response.status_code == 200
 
+@pytest.mark.test_id(5)
+def test_quitter():
+    """
+    """
+    response = requests.post('http://127.0.0.1:5150/quitter')
+    assert response.status_code == 200
+    
 @pytest.mark.test_id(10000)
 def test_last():
     """ test_last """

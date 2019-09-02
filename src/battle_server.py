083320d63bf8da289f6c485b9175e1ca1f88e52e
Lazlo <glebite@gmail.com>
2019-09-02 16:53:48 -0400

    added quit functionality through /quitter endpoint!

--- src/battle_server.py
+++ src/battle_server.py
@@ -1,8 +1,9 @@
 """
 battle_server.py
 """
-from flask import Flask
+from flask import Flask, abort, request
 from flask import jsonify
+import sys
 
 APP = Flask(__name__)
 
@@ -14,5 +15,18 @@ def get_status():
         game_status=""
         )
 
+@APP.route('/quitter', methods=["POST"])
+def quitter():
+    """
+    quitter 
+    """
+    func = request.environ.get('werkzeug.server.shutdown')
+    func()
+    return jsonify(system_status="quitting", game_status="")
+    
 if __name__ == "__main__":
-    APP.run(host='0.0.0.0', port=5150)
+    """
+    debug=True allows for auto-reload on file changes
+    """
+    APP.run(host='0.0.0.0', port=5150, debug=True)
+

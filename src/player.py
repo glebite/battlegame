52b3e318953b4de8c379ff8475040e7b9520d1b0
Lazlo <glebite@gmail.com>
2019-09-06 19:56:55 -0400

    adding in start of player class

---
+++ src/player.py
@@ -0,0 +1,15 @@
+"""
+player.py
+"""
+
+class Player:
+    """ class Player """
+    def __init__(self, name=None):
+        self.name = name
+
+def main():
+    """ main """
+    pass
+
+if __name__=="__main__":
+    main()

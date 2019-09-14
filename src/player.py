7a1de3a93a3cef0b1ae7664d53ac5d40d50abd52
Lazlo <glebite@gmail.com>
2019-09-14 09:00:17 -0400

    adding more to player class and of course tests

--- src/player.py
+++ src/player.py
@@ -6,13 +6,45 @@ from grid import Grid
 
 class Player:
     """ class Player """
+    hits = 0
+    misses = 0
+    enemy_sunk = 0
+    mine_sunk = 0
+
     def __init__(self, name=None):
+        """
+        __init__ - all of the init goodness
+        """
         self.player_name = name
         self.game_grid = Grid()
 
+    def reset_stats(self):
+        """
+        reset_stats - used to reset the user for another game
+        """
+        self.hits = 0
+        self.misses = 0
+        self.enemy_sunk = 0
+        self.mine_sunk = 0
+
+    def player_hits(self):
+        """
+        player_hits - a method to record a hit
+        """
+        self.hits += 1
+
+    def debug_status(self):
+        """
+        debug_status - simple logging output for debug usage
+        """
+        print('Player: {}\n\tHits: {}\n\tMisses: {}\n\t'
+              'Enemy Sunk: {}\n\tMine Sunk: {}\n'.
+              format(self.player_name, self.hits, self.misses,
+                     self.enemy_sunk, self.mine_sunk))
+
 def main():
     """ main """
-    pass
+    return
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
--- test/test_player.py
+++ test/test_player.py
@@ -23,3 +23,21 @@ def test_player_creation_default():
 def test_player_grid_default():
     player_obj = Player()
     assert player_obj.game_grid.rows == 10 and player_obj.game_grid.cols == 10
+
+@pytest.mark.test_id(4)
+def test_player_initial_stats():
+    player_obj = Player()
+    assert player_obj.hits == 0 and player_obj.misses == 0 and player_obj.enemy_sunk == 0 and player_obj.mine_sunk == 0
+
+@pytest.mark.test_id(5)
+def test_player_hits():
+    player_obj = Player()
+    player_obj.player_hits()
+    assert player_obj.hits == 1 and player_obj.misses == 0 and player_obj.enemy_sunk == 0 and player_obj.mine_sunk == 0
+
+@pytest.mark.test_id(6)
+def test_player_reset():
+    player_obj = Player()
+    player_obj.player_hits()
+    player_obj.reset_stats()
+    assert player_obj.hits == 0 and player_obj.misses == 0 and player_obj.enemy_sunk == 0 and player_obj.mine_sunk == 0    

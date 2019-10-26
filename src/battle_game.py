8b3ab1a590a24609837673fd2a9f6ba4a92b9149
Lazlo <glebite@gmail.com>
2019-10-26 14:28:11 -0400

    Changing the battle_game to a client obj and game flow of user input...

--- src/battle_game.py
+++ src/battle_game.py
@@ -9,17 +9,30 @@ import battle_server
 import requests
 import os
 
-def play():
-    params = {'name': 'battle_server'}
-    response = requests.post('http://127.0.0.1:5150/game', data=params)
-    if response.status_code is not 200:
-        print("Problem with starting game...")
-        os.exit(1)
-    player_name = {'player_name': 'MacArthur'}
-    response = requests.post('http://127.0.0.1:5150/player', data=player_name)
+class GameClient:
+    def __init__(self):
+        try:
+            params = {'name': 'battle_server'}
+            response = requests.post('127.0.0.1:5150/game', data=params)
+        except Exception as e:
+            print(e)
+        requests.post('http://127.0.0.1:5150/quitter')
 
-    requests.post('http://127.0.0.1:5150/quitter')
+def play():
+    command = raw_input("Enter your name: ")
+    # params = {'name': 'battle_server'}
+    # response = requests.post('http://127.0.0.1:5150/game', data=params)
+    # if response.status_code is not 200:
+    #     print("Problem with starting game...")
+    # player_name = {'player_name': 'MacArthur'}
+    # response = requests.post('http://127.0.0.1:5150/player', data=player_name)
+    # if response.status_code is not 200:
+    #     print("Problem with creating player")
+    # response = requests.post('http://127.0.0.1:5150/player/fire')
+    # if response.status_code is not 201:
+    #     print("Problem firing a shot")
+    # requests.post('http://127.0.0.1:5150/quitter')
 
     
 if __name__ == "__main__":
-    play()
+    client_obj = GameClient()

0e7d5f8ad8bc691e3bc5d7f3b48ed57ad0e2b8c7
Lazlo <glebite@gmail.com>
2019-11-02 08:41:49 -0400

    adding in getter and setter for the cells

--- src/cell.py
+++ src/cell.py
@@ -2,13 +2,42 @@
 Cell.py
 """
 
+EMPTY = 0
+HIT = 1
+SHIP = 2
+SUNK = 4
 
 class Cell:
     def __init__(self):
-        self.state = 0
-        self.contains_piece_of_ship = None
+        self.__state = EMPTY
+        self.__contains_piece_of_ship = None
 
+    @property
+    def state(self):
+        """ """
+        return self.__state
 
+    @property
+    def contains_piece_of_ship(self):
+        """ """
+        return self.__contains_piece_of_ship
+
+    @state.setter
+    def state(self, value):
+        self.__state = value
+
+    @contains_piece_of_ship.setter
+    def contains_piece_of_ship(self, value):
+        self.__contains_piece_of_ship = value
+        
+    def __str__(self):
+        state_dict = {EMPTY: ' ',
+                      HIT: 'H',
+                      SHIP: 'S',
+                      SUNK: '*'}
+        return state_dict[self.state]
+
+    
 def main():
     pass
 

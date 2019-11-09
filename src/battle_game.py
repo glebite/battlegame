642e105b6f4648b313173dbbcf95778ea36b24d6
Lazlo <glebite@gmail.com>
2019-11-08 19:59:15 -0500

    documentation and pylint corrections - back pain, crashing...

--- src/battle_game.py
+++ src/battle_game.py
@@ -1,45 +1,52 @@
 """
 battle_game.py
 """
+import os
+import requests
 import game
 import grid
 import player
 import ship_piece
 import battle_server
-import requests
-import os
 
 NOT_DONE = 0
 DONE = 1
 
 class GameClient:
+    """ GameClient """
     def __init__(self):
+        """ __init__ """
         self.state = NOT_DONE
         try:
             params = {'name': 'battle_server'}
             response = requests.post('127.0.0.1:5150/game', data=params)
         except Exception as e:
             print(e)
+        return response
 
     def create_player(self, player_name):
+        """ create_player """
         player_info = {'player_name': player_name}
         response = requests.post('http://127.0.0.1:5150/player', data=player_info)
-        return
-    
+        return response
+
     def quit_game(self):
+        """ quit_game """
         response = requests.post('http://127.0.0.1:5150/quitter')
-        return
-    
+        return response
+
     def player_input(self):
+        """ player_input """
         command = raw_input("Enter player command: ")
         if command == "quit":
             self.state = DONE
             self.quit_game()
         return command
 
-    
 def play():
+    """ play """
     command = raw_input("Enter your name: ")
+    print("name = {}".format(command))
     # params = {'name': 'battle_server'}
     # response = requests.post('http://127.0.0.1:5150/game', data=params)
     # if response.status_code is not 200:
@@ -53,9 +60,9 @@ def play():
     #     print("Problem firing a shot")
     # requests.post('http://127.0.0.1:5150/quitter')
 
-    
+
 if __name__ == "__main__":
-    client_obj = GameClient()
-    while client_obj.state is NOT_DONE:
-        command = client_obj.player_input()
-        print("Executing command: {}".format(command))
+    CLIENT_OBJ = GameClient()
+    while CLIENT_OBJ.state is NOT_DONE:
+        COMMAND_INPUT = CLIENT_OBJ.player_input()
+        print("Executing command: {}".format(COMMAND_INPUT))
